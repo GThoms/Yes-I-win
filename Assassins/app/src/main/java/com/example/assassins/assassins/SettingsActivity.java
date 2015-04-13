e005448c9e6f1669bbfb51b5372e00772bbf5647
Caroline Cevallos <lostsingingturtle@gmail.com>
2015-04-13 01:11:25 -0400

    first time trying to add stuff

---
+++ Assassins/app/src/main/java/com/example/assassins/assassins/SettingsActivity.java
@@ -0,0 +1,17 @@
+package com.example.assassins;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//setContentView(R.layout.activity_settings);
+		addPreferencesFromResource(R.xml.mypreferences);
+
+	}
+
+}
