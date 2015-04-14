0fc48482f4ac09117ebaf2c3365adbaa1c8249ec
George Thoms <gscottthoms@gmail.com>
2015-04-14 03:05:05 -0400

    added leaderboard, stats activity

---
+++ Assassin/app/src/main/java/jhua/com/assassin/LeaderBoard.java
@@ -0,0 +1,56 @@
+package jhua.com.assassin;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TabHost;
+
+
+public class LeaderBoard extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leader_board);
+        TabHost tabHost=(TabHost)findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        TabHost.TabSpec spec1 = tabHost.newTabSpec("Eliminated Players");
+        spec1.setIndicator("Eliminated Players"); //Set tab title
+        spec1.setContent(R.layout.eliminate_leaderboard); //Set tab content
+
+
+        TabHost.TabSpec spec2=tabHost.newTabSpec("Remaining Players");
+        spec2.setIndicator("Remaining Players"); //Set tab title
+        spec2.setContent(R.layout.remaining_players_leaderboard);//Set tab content
+
+
+        tabHost.addTab(spec1);
+        tabHost.addTab(spec2);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_leader_board, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
