436f7f9edfba631b2c12c55870d4c4d8c5fcb2ac
unknown <mbotten1@jhu.edu>
2015-05-03 15:05:15 -0400

    Login should work now actually though

--- Assassin/app/src/main/java/com/jhua/assassin/LoginActivity.java
+++ Assassin/app/src/main/java/com/jhua/assassin/LoginActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.parse.LogInCallback;
@@ -18,7 +21,23 @@ public class LoginActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        ParseUser.logInInBackground("Jerry", "showmethemoney", new LogInCallback() {
+        Button login = (Button) findViewById(R.id.button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptlogin();
+            }
+        });
+    }
+
+    private void attemptlogin() {
+        EditText uname = (EditText) findViewById(R.id.editText);
+        EditText pword = (EditText) findViewById(R.id.editText2);
+
+        String name = uname.getText().toString();
+        String pass = uname.getText().toString();
+
+        ParseUser.logInInBackground(name, pass, new LogInCallback() {
             public void done(ParseUser user, ParseException e) {
                 if (user != null) {
                     // Hooray! The user is logged in.
