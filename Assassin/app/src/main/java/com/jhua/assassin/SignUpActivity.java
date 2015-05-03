dc259e2036190cb2bff4942e8f8ede2dbb281f29
rbushko <rbushko1@jhu.edu>
2015-05-03 13:50:18 -0400

    refresh icon/some activity stuff

--- Assassin/app/src/main/java/com/jhua/assassin/SignUpActivity.java
+++ Assassin/app/src/main/java/com/jhua/assassin/SignUpActivity.java
@@ -4,14 +4,45 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignUpActivity extends ActionBarActivity {
+
+    EditText username;
+    EditText password;
+    EditText c_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        c_password = (EditText) findViewById(R.id.c_password);
+
+        ParseUser user = new ParseUser();
+        user.setUsername("my name");
+        user.setPassword("my pass");
+        user.setEmail("email@example.com");
+
+        // other fields can be set just like with ParseObject
+        user.put("phone", "650-253-0000");
+
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    // Hooray! Let them use the app now.
+                } else {
+                    // Sign up didn't succeed. Look at the ParseException
+                    // to figure out what went wrong
+                }
+            }
+        });
     }
 
     @Override
