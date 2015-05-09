ed8a8eca11c407a6e1fb7e7e8b06ce2cdb74a281
unknown <mbotten1@jhu.edu>
2015-05-09 19:58:51 -0400

    fixed add friends

--- Assassin/app/src/main/java/com/jhua/assassin/AddFriendsActivity.java
+++ Assassin/app/src/main/java/com/jhua/assassin/AddFriendsActivity.java
@@ -2,6 +2,7 @@ package com.jhua.assassin;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,16 +26,24 @@ import java.util.ArrayList;
 
 public class AddFriendsActivity extends Activity {
 
+    ListView lv;
+    ArrayList<Friend> friends;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friends);
 
-        final ListView friendList = (ListView) findViewById(R.id.friendList);
+        lv = (ListView) findViewById(R.id.addfriendList);
 
         //Get friend list from current user
         ArrayList<String> sFriends = (ArrayList<String>) ParseUser.getCurrentUser().get("friends");
-        ArrayList<Friend> friends = new ArrayList<Friend>();
+        friends = new ArrayList<Friend>();
+
+        if(sFriends.isEmpty()) {
+            Intent intent = new Intent(AddFriendsActivity.this, CreateGameActivity.class);
+            Toast.makeText(getApplicationContext(),"You do not have any friends to add!",Toast.LENGTH_LONG).show();
+        }
 
         for(String s: sFriends) {
             Friend f = new Friend(s,false);
@@ -41,12 +51,11 @@ public class AddFriendsActivity extends Activity {
         }
 
         //Make a new adapter for friend list
-        final CustomAdapter adapter = new CustomAdapter(getApplicationContext(), R.layout.list_item_checkbox,friends);
+        final CustomAdapter adapter = new CustomAdapter(this, friends);
 
-        friendList.setAdapter(adapter);
         //If user has friends, set adapter to friend list view
         if (friends != null) {
-            adapter.addAll(friends);
+            lv.setAdapter(adapter);
         }
 
         Button addFriends = (Button) findViewById(R.id.addfriendsButton);
@@ -54,6 +63,13 @@ public class AddFriendsActivity extends Activity {
             @Override
             public void onClick(View v) {
                 ArrayList<Friend> friends = adapter.getFriends();
+                String names = "";
+                for(Friend f: friends){
+                    if(f.isSelected()) {
+                        names += f.getName() + " ";
+                    }
+                }
+                Toast.makeText(getApplicationContext(), names, Toast.LENGTH_LONG).show();
                 //send push notifications to the users
                 //add the game to all friends pending games section
             }
@@ -83,9 +99,10 @@ public class AddFriendsActivity extends Activity {
     }
 
 
-    private class CustomAdapter extends ArrayAdapter<Friend> {
+    public class CustomAdapter extends ArrayAdapter<Friend> {
 
         private ArrayList<Friend> friends;
+        private Context context;
 
         public CustomAdapter(Context context, int textViewResourceId, ArrayList<Friend> fList) {
             super(context, textViewResourceId, fList);
@@ -97,6 +114,12 @@ public class AddFriendsActivity extends Activity {
             super(context, textViewResourceId);
         }
 
+        public CustomAdapter(Context context, ArrayList<Friend> friendList) {
+            super(context, R.layout.list_item_checkbox, friendList);
+            this.friends = friendList;
+            this.context = context;
+        }
+
         private class ViewHolder {
             CheckBox name;
         }
@@ -104,28 +127,27 @@ public class AddFriendsActivity extends Activity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
 
-            ViewHolder holder = null;
+            ViewHolder holder = new ViewHolder();
             View row = convertView;
 
             if (row == null) {
-                LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                LayoutInflater vi = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 row = vi.inflate(R.layout.list_item_checkbox, null);
 
-                holder = new ViewHolder();
-                holder.name = (CheckBox) convertView.findViewById(R.id.friend_checkbox);
+                holder.name = (CheckBox) row.findViewById(R.id.friend_checkbox);
                 row.setTag(holder);
 
                 holder.name.setOnClickListener(new View.OnClickListener() {
+                    @Override
                     public void onClick(View v) {
-                        CheckBox cb = (CheckBox) v ;
-                        Friend f = (Friend) cb.getTag();
-                        f.setSelected(cb.isChecked());
+                        CheckBox cb = (CheckBox) v;
+                        Friend friend = (Friend) cb.getTag();
+                        friend.setSelected(cb.isChecked());
                     }
                 });
-
             }
             else {
-                holder = (ViewHolder) convertView.getTag();
+                holder = (ViewHolder) row.getTag();
             }
 
             Friend f = friends.get(position);
@@ -138,7 +160,7 @@ public class AddFriendsActivity extends Activity {
 
         }
 
-        public void addALL(ArrayList<Friend> frnds) {
+        public void addAll(ArrayList<Friend> frnds) {
             this.friends.addAll(frnds);
         }
 
